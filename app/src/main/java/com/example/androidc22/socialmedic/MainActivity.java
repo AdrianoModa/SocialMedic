14704ce46c32c84698f563cfaf0d470abfd9c542
adrianomoda <adrianomodafeitosa@gmail.com>
2017-03-25 23:48:57 -0300

    Criando Intent para TelaMenu

--- app/src/main/java/com/example/androidc22/socialmedic/MainActivity.java
+++ app/src/main/java/com/example/androidc22/socialmedic/MainActivity.java
@@ -1,11 +1,16 @@
 package com.example.androidc22.socialmedic;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button bEntrar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -13,5 +18,15 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         Toast.makeText(this, "Construindo SocialMedic", Toast.LENGTH_LONG).show();
+
+        bEntrar = (Button) findViewById(R.id.btnEntrar);
+        bEntrar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Intent itTelaMenu = new Intent(MainActivity.this, TelaMenu.class);
+                startActivity(itTelaMenu);
+            }
+        });
     }
 }
---
+++ app/src/main/java/com/example/androidc22/socialmedic/TelaMenu.java
@@ -0,0 +1,16 @@
+package com.example.androidc22.socialmedic;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class TelaMenu extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_tela_menu);
+
+
+
+    }
+}
