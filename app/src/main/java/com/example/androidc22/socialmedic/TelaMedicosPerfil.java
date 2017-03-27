692e0cc93fee6711c565b2376bb16ece6af3f4af
adrianomoda <adrianomodafeitosa@gmail.com>
2017-03-26 23:57:54 -0300

    Criação de duas Telas perfis Hospitais e Medicos

---
+++ app/src/main/java/com/example/androidc22/socialmedic/TelaMedicosPerfil.java
@@ -0,0 +1,16 @@
+package com.example.androidc22.socialmedic;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.Toast;
+
+public class TelaMedicosPerfil extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_tela_medicos_perfil);
+
+        Toast.makeText(this, "Tela Medico Perfis", Toast.LENGTH_LONG).show();
+    }
+}
