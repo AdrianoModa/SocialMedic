31a8c60f094cd622b782cf8a276060a8002d8cea
michael.alb <maikonmorais@gmail.com>
2017-03-28 10:39:25 -0300

    Link entre TelaMedicos com TelaMedicosPerfil

--- app/src/main/java/com/example/androidc22/socialmedic/TelaMedicos.java
+++ app/src/main/java/com/example/androidc22/socialmedic/TelaMedicos.java
@@ -1,17 +1,34 @@
 package com.example.androidc22.socialmedic;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.widget.Toast;
+import android.view.View;
+import android.widget.Button;
 
-public class TelaMedicos extends AppCompatActivity {
+public class TelaMedicos extends AppCompatActivity{
+
+    private Button btnMedico;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_tela_medicos);
 
-        Toast.makeText(this, "Tela Médicos OK!", Toast.LENGTH_LONG).show();
+        btnMedico = (Button) findViewById(R.id.btnMedico);
+
+        btnMedico.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v){
+                Intent itTelaMedicoPerfil = new Intent(TelaMedicos.this, TelaMedicosPerfil.class);
+                startActivity(itTelaMedicoPerfil);
+            }
+        });
+
+
+        //Toast.makeText(this, "Tela Médicos OK!", Toast.LENGTH_LONG).show();
 
     }
+
+
 }
